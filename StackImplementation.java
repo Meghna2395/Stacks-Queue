a52b4d0d03bc6edbe1e4a10044d46b40209145e7
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-07-18 23:06:53 +0530

    Add files via upload

---
+++ StackImplementation.java
@@ -0,0 +1,20 @@
+package com.bridzelabz.stack;
+
+public class StackImplementation {
+	
+	public static void main(String[] args) {
+		
+		 Stack stack = new Stack(5);
+		 //stack.pop();
+		 stack.push(56);
+		 stack.push(30);
+		 stack.push(70);
+		 
+		 
+
+		
+
+	
+	}
+
+}
