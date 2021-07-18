bef2e4f7e6803dfbbd704fad90fe2c10abc2a1ef
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-07-18 23:11:22 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,56 @@
+package com.bridzelabz.stack;
+
+public class Stack{
+	
+	int capacity;//10 size of array
+	int [] array; //defining array
+	//T[]array;
+	int top; //-1
+	
+	public Stack(int size) {             //parametorized constructor
+		array = new int [size];
+		this.capacity=size;
+		this.top=-1;
+	}
+	
+	public void push (int element){
+		
+		if(isFull()) {
+			System.out.println("stack is full and can't insert element");
+			System.exit(1);
+		}
+		
+		System.out.println("Inserting "+element+" to the stack");
+		array[++top]=element;              //preincrement 
+	} 
+	
+	public int pop() {
+		if (isEmpty()) {
+			System.out.println("Stack is empty and no space");
+			System.exit(1);
+		}
+		System.out.println("removing the value: "+array[top--]);
+		return array[top--];
+	}
+	
+	public int peek() {
+		if(isEmpty()) {
+			System.exit(1);
+		}else {
+			return array[top];
+		}
+		return -1;
+	}
+	
+	public boolean isEmpty() {
+		return top == -1;
+	}
+	
+	public boolean isFull () {
+		
+		return top== capacity-1;
+	}
+	
+	
+
+}
---
+++ StackImplementation.java
@@ -0,0 +1,21 @@
+package com.bridzelabz.stack;
+
+public class StackImplementation {
+	
+	public static void main(String[] args) {
+		
+		 Stack stack = new Stack(3);
+		
+		 stack.push(56);
+		 stack.push(30);
+		 stack.push(70);
+		 stack.pop();
+		 
+		 System.out.println(stack.peek());
+
+		
+
+	
+	}
+
+}
