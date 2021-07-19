02aa03700a30e3fc6b1383747c6179653906ae6d
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-07-19 20:53:39 +0530

    Add files via upload

---
+++ QueueTest.java
@@ -0,0 +1,54 @@
+package com.bridzelabz.queue;
+
+public class QueueTest {
+	int capacity;
+	int array[];
+	int front;
+	int rear;
+	int size = 0;
+
+	public void Queue(int capacity) {
+		this.capacity = capacity;
+		front = 0;
+		array = new int[capacity];
+
+	}
+
+	public void enqueue(int element) {
+		if (isFull()) {
+			System.out.println("Queue is  not accepting any further element");
+
+		} else {
+			System.out.println("Current Size :" + size);
+			size++;
+			System.out.println("Stack has :");
+		}
+	}
+	
+	public void dequeue() {
+		if (size == 0) {
+			System.out.println("Queue is Empty so cant delete the element");
+		} else {
+			front++;
+			if (front == capacity - 1) {
+				int DeletedElement = array[front - 1];
+				System.out.println("Deleted Element :" + DeletedElement);
+				front = 0;
+			} else {
+				int DeletedElement = array[front - 1];
+				System.out.println("Deleted Element :" + DeletedElement);
+			}
+
+			size--;
+			rear--;
+		}
+
+	}
+
+	public boolean isFull() {
+		if (size == capacity) {
+			return true;
+		}
+		return false;
+	}
+}
