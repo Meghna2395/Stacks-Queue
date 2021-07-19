bc3f613527e7f2130edab40c205483912cd8b4fc
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-07-19 20:47:09 +0530

    Add files via upload

---
+++ QueueuImp.java
@@ -0,0 +1,11 @@
+package com.bridzelabz.queue;
+
+public class QueueuImp {
+	public static void main(String[] args) {
+		Queue queue = new Queue(3);
+		queue.enqueue(56);
+		queue.enqueue(30);
+		queue.enqueue(70);
+		queue.enqueue(40);
+	}
+}
